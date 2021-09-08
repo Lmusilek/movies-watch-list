d093cc1bf3e1d417f14d259e7dc028e1d1f2dc89
Lukas Musilek <lukas.musilek@gmail.com>
2021-09-08 20:27:31 +0100

    changed double quotes to single in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,17 @@
 require 'open-uri'
 require 'json'
 
-puts "Cleaning up database..."
+puts 'Cleaning up database...'
 Movie.destroy_all
-puts "Database cleaned"
+puts 'Database cleaned'
 
-url = "http://tmdb.lewagon.com/movie/top_rated"
+url = 'http://tmdb.lewagon.com/movie/top_rated'
 10.times do |i|
   puts "Importing movies from page #{i + 1}"
   movies = JSON.parse(open("#{url}?page=#{i + 1}").read)['results']
   movies.each do |movie|
     puts "Creating #{movie['title']}"
-    base_poster_url = "https://image.tmdb.org/t/p/original"
+    base_poster_url = 'https://image.tmdb.org/t/p/original'
     Movie.create(
       title: movie['title'],
       overview: movie['overview'],
@@ -27,4 +27,4 @@ url = "http://tmdb.lewagon.com/movie/top_rated"
     )
   end
 end
-puts "Movies created"
+puts 'Movies created'
